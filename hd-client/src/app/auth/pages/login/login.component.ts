4da930521cc70b0dee06a2aadf62dee09b66d143
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2024-06-25 15:11:59 -0300

    Login Container Form

--- hd-client/src/app/auth/pages/login/login.component.ts
+++ hd-client/src/app/auth/pages/login/login.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup
+
+  showPassword: boolean = false
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _snackBar: MatSnackBar
+  ) {
+    this.loginForm = this._formBuilder.group({
+      username: [null],
+      password: [null]
+    })
+  }
+
+  ngOnInit(): void { }
+
+  changePasswordVisibility() {
+    this.showPassword = !this.showPassword
+  }
+
+  onLoginSubmit() {
+    this.showSnackBar('Login não implementado!!!', 'Ok!', 3000)
+  }
+
+  onForgotPasswordSubmit() {
+    this.showSnackBar('Esqueceu a senha não implementado!!!', 'Ok!', 3000)
+  }
+  
+  private showSnackBar(message: string, action: string, snackBarDuration: number) {
+    this._snackBar.open(message, action, { duration: snackBarDuration })
+  }
 
 }
--- hd-client/src/app/shared/angular-material/angular-material.module.ts
+++ hd-client/src/app/shared/angular-material/angular-material.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
   exports: [
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class AngularMaterialModule { }
